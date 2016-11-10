2c55da90f792d0bc3c59f82b1ed3db6658d3967f
marvi154 <margareta.vi@gmail.com>
2016-11-10 10:58:34 +0100

    now it runs on multicore

--- sort_image.py
+++ sort_image.py
@@ -4,34 +4,45 @@
 import numpy as np
 import cv2, shutil, sys, os
 import time
+from multiprocessing import Pool
 
-def main(argv):
-	path_to_folder = sys.argv[1]
-	dest_path = sys.argv[2]
-	os.path.dir(path_to_folder)
-	os.path.dir(dest_path)
+path_to_folder = sys.argv[1]
+dest_path = sys.argv[2]
+thres_blue = 0.4
+thres_green = 0.3
+thres_both = 0.6
+height = 512
+width = 512
+tot_pixels = height*width
+
+def main():
+
+	#os.path.dir(path_to_folder)
+	#os.path.dir(dest_path)
 	#Thresholds can be changed.
-	thres_blue = 0.4
-	thres_green = 0.3
-	thres_both = 0.6
-	height = 512
-	width = 512
-	tot_pixels = height*width
-	t0 = time.time()
-	for image in os.listdir(path_to_folder):
-		# If the images is not in the .png format just add the condition to the following if-statement with the extention of file.
-		if image.endswith('.png'):
-			path_to_image = path_to_folder + '/' + image
-			img = cv2.imread(path_to_image)
-			no_blue = count_pixels(img,height,width)
-			percent_blue = no_blue/float(tot_pixels) 
-			#percent_green = no_green/float(tot_pixels)
-			#percent_bad = (no_green + no_blue)/float(tot_pixels)
-			#print percent_blue, percent_green
-			if percent_blue > thres_blue:
-				shutil.move(path_to_image, dest_path)
-	t1 = time.time()
-	print t1-t0
+	
+	#t0 = time.time()
+	images = os.listdir(path_to_folder)
+	pool = Pool()
+	pool.map(work, images)
+	pool.close()
+	pool.join()
+	#t1 = time.time()
+	#print t1-t0
+
+#sends in a list of images 
+def work(image):
+# If the images is not in the .png format just add the condition to the following if-statement with the extention of file.
+	if image.endswith('.png'):
+		path_to_image = path_to_folder + '/' + image
+		img = cv2.imread(path_to_image)
+		no_blue = count_pixels(img,height,width)
+		percent_blue = no_blue/float(tot_pixels) 
+	#percent_green = no_green/float(tot_pixels)
+	#percent_bad = (no_green + no_blue)/float(tot_pixels)
+	#print percent_blue, percent_green
+	if percent_blue > thres_blue:
+		shutil.move(path_to_image, dest_path)
 
 def count_pixels(img, height, width):
 	no_blue_pixels = 0
@@ -47,4 +58,4 @@ def count_pixels(img, height, width):
 
 
 if __name__ == '__main__':
-	main(sys.argv[1:])
+	main()
