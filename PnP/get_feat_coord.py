95749c6e70b8acb83225a4eb3aaa79cf7735533a
marvi154 <margareta.vi@gmail.com>
2016-11-11 16:55:56 +0100

    added rendered camerapos in 2D corresp array

--- PnP/get_feat_coord.py
+++ PnP/get_feat_coord.py
@@ -1,25 +1,33 @@
 #This scrip is going to find the homogenous coordinates of the matching 2D corresponding points of two images.
 # It takes two images as inargument and returns the coordinates of the matching points in respective image
-import cv2, os, sys
+import cv2, os, sys, re
 import numpy as np
 import pdb
 from tempfile import TemporaryFile
 
 # User input it text file with narrative camera parameters, location to queryImage and trainImage, primary camera pos from NN:
 def main(argv):
+	image1 = sys.argv[1]
+	image2 = sys.argv[2]
 
-	# uncomment these when the code are working, the user should give the script two images
-	#image1 = sys.argv[1]
-	#image2 = sys.argv[2]
+	img1 = cv2.imread(image1,0)
+	img2 = cv2.imread(image2,0)
+	name1 = get_file_name(image1)
+	name2 = get_file_name(image2)
 
-	img1 = cv2.imread('../bilder/TRtest_x104_y24_a0_t-3.png',0) # queryImage, grayscale
-	img2 = cv2.imread('../bilder/TRtest_x104_y24_a352_t-25.png',0) # trainImage, grayscale
+	x_pos1, y_pos1, a_pos1, t_pos1 = get_render_camerapose(name1)
+	x_pos2, y_pos2, a_pos2, t_pos2 = get_render_camerapose(name2)
+
 	coord_img1, coord_img2 = corresponding_twoD_points(img1, img2)
-	#queryImage_name = os.path.basename(image1)
-	#trainImage_name = os.path.basename(image2)
+	coord_img1[0,:] = [x_pos1, y_pos1,0]
+	coord_img1[1,:] = [a_pos1, t_pos1,0]
 
-	save_to_file('train', coord_img2)
-	save_to_file('query', coord_img1)
+	coord_img2[0,:] = [x_pos2, y_pos2,0]
+	coord_img2[1,:] = [a_pos2, t_pos2,0]
+
+	save_to_file(name1, coord_img1)
+	save_to_file(name2, coord_img2)
+	
 
 # Takes two images, one rendered from the camera pose in the 3D model
 # and the other one is the image taken with the camera, the two images has the same size
@@ -28,6 +36,8 @@ def corresponding_twoD_points(rendered_image, real_image):
 
 	img1 = rendered_image
 	img2 = real_image
+	#camera_pos1 =
+	#camera_pos2 = 
 	# Create ORB detector with 1000 keypoints with a scaling pyramid factor
 	# of 1.2
 	orb = cv2.ORB(1000, 2)
@@ -48,17 +58,17 @@ def corresponding_twoD_points(rendered_image, real_image):
 
 	# convert the matches to coordinates in image
 	coord_img1, coord_img2 = get_coordinates(matches, kp1, kp2)
-
 	return coord_img1, coord_img2
 
 # Converts the matching points to homogenous (x,y) coordinated
 # Returns homogenous 2D coordninates
 def get_coordinates(matches, kp1, kp2):
 	lenght_mat = len(matches)
-	array_kp1 = np.empty([lenght_mat,3])
-	array_kp2 = np.empty([lenght_mat,3])
+
+	array_kp1 = np.empty([lenght_mat+2,3]) # Add 2 due to x,y,angle ,and tilt of camera
+	array_kp2 = np.empty([lenght_mat+2,3])
 	# Loop through all the matches
-	i = 0
+	i = 2
 	for match in matches:
 
 	# Get the matching keypoints for each of the images
@@ -71,7 +81,7 @@ def get_coordinates(matches, kp1, kp2):
 		x1,y1 = kp1[img1_idx].pt
 		x2,y2 = kp2[img2_idx].pt
 
-	# Append to each list (homogenous coordinates)
+	# Append to each list , homogenous coordiates
 		array_kp1[i,:] = [x1,y1,1]
 		array_kp2[i,:] = [x2,y2,1]
 		i = i +1
@@ -80,6 +90,30 @@ def get_coordinates(matches, kp1, kp2):
 # Save the correpsonding 2D points of each image to file with same name
 def save_to_file(file_name, coordinates):
 	np.save(file_name, coordinates)
+
+def get_file_name(image):
+	whole_name = os.path.basename(image)
+	name, ext = os.path.splitext(whole_name)
+	return name
+
+# Get the camera pose which the image is rendered with
+def get_render_camerapose(name):
+	x = re.compile('_x(\d+)')
+	x_pos = x.findall(name)
+	x_pos = int(x_pos[0])
+	
+	y = re.compile('_y(\d+)')
+	y_pos = y.findall(name)
+	y_pos = int(y_pos[0])
+
+	a = re.compile('_a(\d+)')
+	a_pos = a.findall(name)
+	a_pos = int(a_pos[0])
+
+	t = re.compile('_t(.*)')
+	t_pos = t.findall(name)
+	t_pos = int(t_pos[0])
+	return x_pos, y_pos, a_pos, t_pos
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
