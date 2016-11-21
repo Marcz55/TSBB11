27f26b4eed7acd38da4ede0c38dcb8348e5e40f8
marvi154 <margareta.vi@gmail.com>
2016-11-21 15:25:13 +0100

    add grid type feature

--- PnP/correct_camerapos.py
+++ PnP/correct_camerapos.py
@@ -12,7 +12,7 @@ def main(argv):
 	twoD_tmp = twoD_tmp[2:,:]
 	len_twoD = len(twoD_tmp)
 	len_threeD = len(threeD_tmp)
-
+	#print "3D points:", threeD_tmp
 	if len_threeD != len_twoD:
 		print "lenght of 3D corresp:" + str(len_threeD)
 		print "lenght of 2D corresp:" + str(len_twoD)
@@ -20,7 +20,6 @@ def main(argv):
 
 	twoD_corres = copy_array(twoD_tmp,len_twoD,2)
 	threeD_corres = copy_array(threeD_tmp,len_threeD,3)
-
 	dist_Coeffs = np.zeros((5,1))
 
 	retval, rvecs, tvecs = cv2.solvePnP(threeD_corres, twoD_corres, camera_mtx, dist_Coeffs)
@@ -28,12 +27,16 @@ def main(argv):
 	# project 3D points to image plane
 	imgpts, jac = cv2.projectPoints(threeD_corres, rvecs, tvecs, camera_mtx, dist_Coeffs)
 
-	rvec_ransac, tvec_ransac, inliers = cv2.solvePnPRansac(threeD_corres,twoD_corres, camera_mtx, dist_Coeffs,rvecs,tvecs,1,1500, 1) 
+	# Obtains the camera position 
+	cam_pose, camera_pose = calc_camera_pose(rvecs, tvecs)
+	print "camerapose1:", camera_pose
+	print "camerapose2:", cam_pose
+	#print "Rotation matrix:", rmatrix
+
+	#RANSAC
+	rvec_ransac, tvec_ransac, inliers = cv2.solvePnPRansac(threeD_corres,twoD_corres, camera_mtx, dist_Coeffs,rvecs,tvecs,1,1500) 
 
 	imgpts_ransac, jac_ransac = cv2.projectPoints(threeD_corres,rvec_ransac,tvec_ransac,camera_mtx,dist_Coeffs)
-
-	#threeD_corres_reshape = np.reshape(threeD_corres, (-1,3,1))
-	#twoD_corres_reshape = np.reshape(twoD_corres, (-1,2,1))
 
 	# Calculate the mean projection error
 	mean_error = mean_reprojection_error_all(imgpts,twoD_corres,threeD_corres)
@@ -42,19 +45,20 @@ def main(argv):
 	if inliers is not None:
 		mean_reprojection_inliers = mean_reprojection_error_inliers(imgpts_ransac,twoD_corres,threeD_corres,inliers)
 		print "Mean repoj error inliers:", mean_reprojection_inliers
+		print "Numbers of inliners", len(inliers)
 	else:
 		print "No inlier exist"
 	#print "Mean reprojection error:", mean_error
-	#print "Mean reprojection error RANSAC:", mean_error_ransac
+	#print "Mean reprojection error RANSAC w outliers:", mean_error_ransac
 
+	#RANSAC
 	# Obtains the camera position 
-	np_rodrigues = np.asarray(rvecs[:,:],np.float64)
-	rmatrix = cv2.Rodrigues(np_rodrigues)[0]
-	cam_pos = -np.matrix(rmatrix).T * np.matrix(tvecs)
-	camera_pose = -rmatrix.dot(tvecs)
-	print "camerapose1:", camera_pose
-	print  "camerapose2:", cam_pos
+	cam_pose_ransac, camera_pose_ransac = calc_camera_pose(rvec_ransac, tvec_ransac)
+	print "cam_pose_ransac:", cam_pose_ransac
+	print "camera_pose_ransac:", camera_pose_ransac
 
+
+# copy the loaded npy array to a new npy array
 def copy_array(array, len_of_array, dim):
 	tmp_array = np.zeros((len_of_array,dim))
 	for i in range(0,len_of_array):
@@ -82,6 +86,13 @@ def mean_reprojection_error_inliers(repojection_points, ground_truth,corresp_3D,
 	mean_error=np.sqrt(tot_error/total_points)
 	return mean_error
 
+#calculate the camera pose, two ways
+def calc_camera_pose(rvec, tvec):
+	np_rodrigues = np.asarray(rvec[:,:],np.float64)
+	rmatrix = cv2.Rodrigues(np_rodrigues)[0]
+	cam_pose = -np.matrix(rmatrix).T * np.matrix(tvec)
+	camera_pose = -rmatrix.dot(tvec)
+	return cam_pose, camera_pose
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
--- PnP/get_feat_coord.py
+++ PnP/get_feat_coord.py
@@ -1,15 +1,18 @@
 #This scrip is going to find the homogenous coordinates of the matching 2D corresponding points of two images.
 # It takes two images as inargument and returns the coordinates of the matching points in respective image
-import cv2, os, sys, re
+import cv2, os, sys, re, pdb
 import numpy as np
-import pdb
 from tempfile import TemporaryFile
 from draw_matches import * 
 
 # location to queryImage and trainImage, primary camera pos from NN:
 def main(argv):
-	image1 = sys.argv[1]
-	image2 = sys.argv[2]
+	#image1 = sys.argv[1]
+	#image2 = sys.argv[2]
+
+
+	image1 = '/Users/samanthavi/Documents/TSBB11/TSBB11---Camera-poses/PnP/testbilder/Maggiebild_x155_y101_z2_a78_t4.png'
+	image2 = '/Users/samanthavi/Documents/TSBB11/TSBB11---Camera-poses/PnP/testbilder/Maggiebild_x153_5752_y98_25534_z1_45346_a78_203_t0_548.png'
 
 	img1 = cv2.imread(image1,0)
 	img2 = cv2.imread(image2,0)
@@ -20,15 +23,15 @@ def main(argv):
 	x_pos2, y_pos2, z_pos2, a_pos2, t_pos2 = get_render_camerapose(name2)
 
 	coord_img1, coord_img2 = corresponding_twoD_points(img1, img2)
-	coord_img1[0,:] = [x_pos1, y_pos1,0]
+	coord_img1[0,:] = [x_pos1, y_pos1, z_pos1]
 	coord_img1[1,:] = [a_pos1, t_pos1,0]
 
-	coord_img2[0,:] = [x_pos2, y_pos2,0]
+	coord_img2[0,:] = [x_pos2, y_pos2, z_pos2]
 	coord_img2[1,:] = [a_pos2, t_pos2,0]
 
 	save_to_file(name1, coord_img1)
 	save_to_file(name2, coord_img2)
-	
+
 
 # Takes two images, one rendered from the camera pose in the 3D model
 # and the other one is the image taken with the camera, the two images has the same size
@@ -37,16 +40,30 @@ def corresponding_twoD_points(rendered_image, real_image):
 
 	img1 = rendered_image
 	img2 = real_image
-	#camera_pos1 =
-	#camera_pos2 = 
-	# Create ORB detector with 1000 keypoints with a scaling pyramid factor
-	# of 1.2
+
+	'''
+	# Create ORB detector
 	orb = cv2.ORB(2000, 2)
 	# Detect keypoints of original image
 	(kp1,des1) = orb.detectAndCompute(img1, None)
-	# Detect keypoints of rotated image
+	# Detect keypoints of rendered image
 	(kp2,des2) = orb.detectAndCompute(img2, None)
+	'''
+	surf = cv2.FeatureDetector_create('SURF')
+	surfdetector = cv2.GridAdaptedFeatureDetector(surf,50)
 
+	# Detect features on input images
+	kp1 = surfdetector.detect(img1)
+	kp2 = surfdetector.detect(img2)
+
+	# Create a descriptor extractor
+	extractor = cv2.DescriptorExtractor_create('SURF')
+
+	# Calculate descriptors
+	descriptor1 = extractor.compute(img1, kp1)
+	descriptor2 = extractor.compute(img2, kp2)
+
+	'''
 	# Create matcher
 	bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
@@ -55,6 +72,22 @@ def corresponding_twoD_points(rendered_image, real_image):
 
 	# Sort the matches based on distance.  Least distance
 	# is better
+	matches = sorted(matches, key=lambda val: val.distance)
+	out = drawMatches(img1, kp1, img2, kp2, matches[:20])
+	'''
+
+
+	# Match descriptor vectors using FLANN match
+	FLANN_INDEX_KDTREE = 1
+	flann_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+
+	matcher = cv2.FlannBasedMatcher(flann_params, {})
+	matches = matcher.match(descriptor1[1], descriptor2[1])
+	for match in matches:
+		distance = match.distance
+		if distance < 0.2:
+			print distance
+
 	matches = sorted(matches, key=lambda val: val.distance)
 	out = drawMatches(img1, kp1, img2, kp2, matches[:20])
 	# convert the matches to coordinates in image
@@ -66,7 +99,7 @@ def corresponding_twoD_points(rendered_image, real_image):
 def get_coordinates(matches, kp1, kp2):
 	lenght_mat = len(matches)
 
-	array_kp1 = np.empty([lenght_mat+2,3]) # Add 2 due to x,y,angle ,and tilt of camera
+	array_kp1 = np.empty([lenght_mat+2,3]) # Add 2 due to x,y,z, angle ,and tilt of camera
 	array_kp2 = np.empty([lenght_mat+2,3])
 	# Loop through all the matches
 	i = 2
@@ -76,8 +109,7 @@ def get_coordinates(matches, kp1, kp2):
 		img1_idx = match.queryIdx
 		img2_idx = match.trainIdx
 
-	# x - columns
-	# y - rows
+	# x - columns, y - rows.s
 	# Get the coordinates
 		x1,y1 = kp1[img1_idx].pt
 		x2,y2 = kp2[img2_idx].pt
@@ -87,7 +119,7 @@ def get_coordinates(matches, kp1, kp2):
 		i = i +1
 	return array_kp1, array_kp2
 
-# Save the correpsonding 2D points of each image to file with same name
+# Save the correpsonding 2D points of each image to file with same name as .npy files
 def save_to_file(file_name, coordinates):
 	np.save(file_name, coordinates)
 
