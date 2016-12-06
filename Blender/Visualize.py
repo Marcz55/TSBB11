8a6496915dd4d9aeedd2a21b16160aab9f38f719
Marcz55 <marwa828@student.liu.se>
2016-12-06 11:58:59 +0100

    Lite nyare - lite bättre

--- Blender/Visualize.py
+++ Blender/Visualize.py
@@ -2,17 +2,20 @@
 Render a camera in the model at specified coordinates
 '''
 import bpy, math
+from mathutils import Vector
 
 #Start of setup-------------------------------------------------------------------------------
 
-xpos = 153
-ypos = 98
-zpos = 1.94
-xrot = 90
-yrot = 0.54
-zrot = 78.2
+xpos = 220
+ypos = 60
+tilt = -20
+angle = 30
+cameraHeight = 4
+targetModelName = "teknikringen8"
 
 #End of setup---------------------------------------------------------------------------------
+
+targetModel = bpy.data.objects[targetModelName]
 
 bpy.ops.mesh.primitive_cone_add(location=(0,0,0),rotation = (math.radians(90),math.radians(45),math.radians(-90)),vertices = 4,radius1 = 1.2, radius2 = 0)
 part1 = bpy.context.selected_objects
@@ -21,8 +24,15 @@ part1[0].select = True
 bpy.ops.object.join()
 cameraIndicator = bpy.context.selected_objects[0]
 
-cameraIndicator.location = (xpos,ypos,zpos)
-cameraIndicator.rotation_euler = (math.radians(xrot),math.radians(yrot),math.radians(zrot+90))
+#Place camera in desired location and put at correct height over the ground
+bpy.context.scene.objects.active = cameraIndicator
+bpy.context.active_object.location = [xpos,ypos,0]
+localCameraObject = targetModel.matrix_world.inverted()*Vector((xpos,ypos,0))
+depthCast = targetModel.ray_cast([localCameraObject[0],localCameraObject[1],100],[0,0,-1])
+depthCastGlobal = targetModel.matrix_world*depthCast[1]
+
+cameraIndicator.location = (xpos,ypos,depthCastGlobal[2] + cameraHeight)
+cameraIndicator.rotation_euler = (math.radians(tilt),math.radians(0),math.radians(angle+90))
 
 
 
