e59c0cfcebf92ceaa9c861dcd589e046d485ddb1
marvi154 <margareta.vi@gmail.com>
2016-12-07 16:13:19 +0100

    add new functionalties in extracting features and testpoints

--- PnP/get_feat_coord.py
+++ PnP/get_feat_coord.py
@@ -4,33 +4,31 @@ import cv2, os, sys, re, pdb
 import numpy as np
 from tempfile import TemporaryFile
 from draw_matches import * 
+from resizeimage import resizeimage
+from matplotlib import pyplot as plt
 
 # location to queryImage and trainImage, primary camera pos from NN:
 def main(argv):
-	image1 = sys.argv[1] # real
-	image2 = sys.argv[2] # rendered
-	#image1 = '/Users/samanthavi/Documents/TSBB11/TSBB11---Camera-poses/PnP/testbilder/Maggiebild_x155_y101_z2_a78_t4.png'
-	#image2 = '/Users/samanthavi/Documents/TSBB11/TSBB11---Camera-poses/PnP/testbilder/Maggiebild_x153_5752_y98_25534_z1_45346_a78_203_t0_548.png'
+	#image1 = sys.argv[1] # real
+	#image2 = sys.argv[2] # rendered
+	image1 = '/Users/samanthavi/Documents/TSBB11/TSBB11---Camera-poses/PnP/20161003_111538_00009.31.18_small.jpg'
+	image2 = '/Users/samanthavi/Documents/TSBB11/TSBB11---Camera-poses/PnP/Test_x220_y66_z3_a5_t3.png'
 
 	img1 = cv2.imread(image1,0)
 	img2 = cv2.imread(image2,0)
 	name1 = get_file_name(image1)
 	name2 = get_file_name(image2)
 
-	kernel = np.ones((5,5),np.float32)/25
-	img1_filtered = cv2.filter2D(img1,-1,kernel)
-
-	x_pos1, y_pos1, z_pos1, a_pos1, t_pos1 = get_render_camerapose(name1)
+	#x_pos1, y_pos1, z_pos1, a_pos1, t_pos1 = get_render_camerapose(name1)
 	x_pos2, y_pos2, z_pos2, a_pos2, t_pos2 = get_render_camerapose(name2)
 
-	coord_img1, coord_img2 = corresponding_twoD_points(img1_filtered, img2)
-	coord_img1[0,:] = [x_pos1, y_pos1, z_pos1]
-	coord_img1[1,:] = [a_pos1, t_pos1,0]
+	coord_img1, coord_img2 = corresponding_twoD_points(img1, img2)
+
+	#coord_img1[0,:] = [x_pos1, y_pos1, z_pos1]
+	#coord_img1[1,:] = [a_pos1, t_pos1,0]
 
 	coord_img2[0,:] = [x_pos2, y_pos2, z_pos2]
 	coord_img2[1,:] = [a_pos2, t_pos2,0]
-
-	print len(coord_img1)
 
 	save_to_file(name1, coord_img1)
 	save_to_file(name2, coord_img2)
@@ -43,85 +41,58 @@ def corresponding_twoD_points(rendered_image, real_image):
 	img1 = rendered_image
 	img2 = real_image
 
-	
-	# Create ORB detector
-	orb = cv2.ORB(2000, 2)
-	# Detect keypoints of original image
-	(kp1,des1) = orb.detectAndCompute(img1, None)
-	# Detect keypoints of rendered image
-	(kp2,des2) = orb.detectAndCompute(img2, None)
+	# Initiate SIFT detector
+	sift = cv2.xfeatures2d.SIFT_create()
 
-	# Create matcher
-	bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+	# find the keypoints and descriptors with SIFT
+	kp1, des1 = sift.detectAndCompute(img1,None)
+	kp2, des2 = sift.detectAndCompute(img2,None)
 
-	# Do matching
-	matches = bf.match(des1,des2)
+	# FLANN parameters
+	FLANN_INDEX_KDTREE = 0
+	index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+	search_params = dict(checks=100)   # or pass empty dictionary
 
-	# Sort the matches based on distance.  Least distance
-	# is better
-	matches = sorted(matches, key=lambda val: val.distance)
-	out = drawMatches(img1, kp1, img2, kp2, matches[:20])
-	
-	'''
-	surf = cv2.FeatureDetector_create('SURF')
-	surfdetector = cv2.GridAdaptedFeatureDetector(surf,7000)
+	flann = cv2.FlannBasedMatcher(index_params,search_params)
 
-	# Detect features on input images
-	kp1 = surfdetector.detect(img1)
-	kp2 = surfdetector.detect(img2)
-	#tmp = cv2.drawKeypoints(img1,kp1)
-
-	#cv2.imshow('fj',tmp)
-	#tmp2 = cv2.drawKeypoints(img2,kp2)
-
-	#cv2.imshow('fj2',tmp2)
-	#cv2.waitKey(0)
-	# Create a descriptor extractor
-	extractor = cv2.DescriptorExtractor_create('SURF')
-
-	# Calculate descriptors
-	descriptor1 = extractor.compute(img1, kp1)
-	descriptor2 = extractor.compute(img2, kp2)
-
-	# Match descriptor vectors using FLANN match
-	FLANN_INDEX_KDTREE = 1
-	flann_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-
-	matcher = cv2.FlannBasedMatcher(flann_params, {})
-	matches = matcher.match(descriptor1[1], descriptor2[1])
-	matches = sorted(matches, key=lambda val: val.distance)
-	out = drawMatches(img1, kp1, img2, kp2, matches[:20])
-	'''
-
+	matches = flann.knnMatch(des1,des2,k = 4)
 	# convert the matches to coordinates in image
-	coord_img1, coord_img2 = get_coordinates(matches, kp1, kp2)
+	query_Idx, train_Idx = get_matching_index(matches)
+	coord_img1, coord_img2 = get_coordinates(kp1,kp2,query_Idx,train_Idx)
 	return coord_img1, coord_img2
 
-# Converts the matching points to homogenous (x,y) coordinated
-# Returns homogenous 2D coordninates
-def get_coordinates(matches, kp1, kp2):
-	lenght_mat = len(matches)
+# Append all the matches to two lists
+# returns two lists with indices
+def get_matching_index(matches):
+	train_Idx = []
+	query_Idx = []
+	for match in matches:
+		#for i in range(0,len(match)):
+		i = 0
+		train = match[i].trainIdx
+		query = match[i].queryIdx
+		query_Idx.append(query)
+		train_Idx.append(train)	
+	return query_Idx, train_Idx
+
+# Gets the coordinates for the matchings points in each image
+# returns two arrays with coordinates
+def get_coordinates(kp1, kp2, img1_idx, img2_idx):
+	lenght_mat = len(img1_idx)
 
 	array_kp1 = np.empty([lenght_mat+2,3]) # Add 2 due to x,y,z, angle ,and tilt of camera
 	array_kp2 = np.empty([lenght_mat+2,3])
 	# Loop through all the matches
-	i = 2
-	for match in matches:
+	e = 2
+	for i in range(0,lenght_mat):
+		x1,y1 = kp1[img1_idx[i]].pt
+		x2,y2 = kp2[img2_idx[i]].pt
 
-	# Get the matching keypoints for each of the images
-		img1_idx = match.queryIdx
-		img2_idx = match.trainIdx
-
-	# x - columns, y - rows.s
-	# Get the coordinates
-		x1,y1 = kp1[img1_idx].pt
-		x2,y2 = kp2[img2_idx].pt
-	# Append to each list , homogenous coordiates
-		array_kp1[i,:] = [x1,y1,1]
-		array_kp2[i,:] = [x2,y2,1]
-		i = i +1
+		# Append to each list , homogenous coordiates
+		array_kp1[e,:] = [x1,y1,1]
+		array_kp2[e,:] = [x2,y2,1]
+		e += 1
 	return array_kp1, array_kp2
-
 # Save the correpsonding 2D points of each image to file with same name as .npy files
 def save_to_file(file_name, coordinates):
 	np.save(file_name, coordinates)
